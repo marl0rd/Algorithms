93e37a3554f757244894f35a577552ac7b8f59c4
trujillo <marlontrujillo1080@gmail.com>
2016-11-01 17:04:17 -0400

    Initial commit

---
+++ src/QuickFindUF.cpp
@@ -0,0 +1,5 @@
+//
+// Created by trujillo on 01/11/16.
+//
+
+#include "QuickFindUF.h"
---
+++ src/QuickFindUF.h
@@ -0,0 +1,14 @@
+//
+// Created by trujillo on 01/11/16.
+//
+
+#ifndef ALGORITHMS_QUICKFINDUF_H
+#define ALGORITHMS_QUICKFINDUF_H
+
+
+class QuickFindUF {
+
+};
+
+
+#endif //ALGORITHMS_QUICKFINDUF_H
