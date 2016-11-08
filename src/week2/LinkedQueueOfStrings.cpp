9bb87dbd8f95be129619d6c88905f6b7ef140e09
trujillo <marlontrujillo1080@gmail.com>
2016-11-08 09:42:17 -0400

    LinkedStackOfStrings with resizing added.

---
+++ src/week2/LinkedQueueOfStrings.cpp
@@ -0,0 +1,5 @@
+//
+// Created by trujillo on 08/11/16.
+//
+
+#include "LinkedQueueOfStrings.h"
---
+++ src/week2/LinkedQueueOfStrings.h
@@ -0,0 +1,14 @@
+//
+// Created by trujillo on 08/11/16.
+//
+
+#ifndef ALGORITHMS_LINKEDQUEUEOFSTRINGS_H
+#define ALGORITHMS_LINKEDQUEUEOFSTRINGS_H
+
+
+class LinkedQueueOfStrings {
+
+};
+
+
+#endif //ALGORITHMS_LINKEDQUEUEOFSTRINGS_H
